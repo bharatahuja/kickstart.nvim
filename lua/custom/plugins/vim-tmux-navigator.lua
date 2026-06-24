8788ded79ee17fb578f086d5016c4927b9bd6b52
Bharat Ahuja <bharatahuja@live.com>
2026-06-24 14:56:58 +0530

    Get Tmux Nav working again

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -7,19 +7,18 @@
 ---@type LazySpec
 
 return {
-  'christoomey/vim-tmux-navigator',
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
-  },
-  keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-  },
+  'alexghergh/nvim-tmux-navigation',
+  config = function()
+    require('nvim-tmux-navigation').setup {
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = '<M-h>',
+        down = '<M-j>',
+        up = '<M-k>',
+        right = '<M-l>',
+        last_active = '<M-\\>',
+        next = '<M-Space>',
+      },
+    }
+  end,
 }
